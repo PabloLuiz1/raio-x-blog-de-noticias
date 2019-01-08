cf82ee0bb8ca0fae4d3dbd62c9888182ba3bb931
Pablo Luiz Ribeiro <pabloluiz.ribeiroo@gmail.com>
2019-01-08 04:41:45 -0200

    Primeiro commit
    - Estruturação do header e footer das páginas;
    - Criação de diretórios e arquivos;
    + Falta fazer o dropdown funcionar.

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Página inicial</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/font-awesome.min.css" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <link rel="shortcut icon" type="image/png" href="images/Favicon.png"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    </head>
+<body>
+    <div class="container-fluid">
+        <header>
+            <figure class="figure-header">
+                <a href="index.php">
+                    LOGO DO BLOG
+                </a>
+            </figure>
+            <h2 class="title-header">BLOG DE NOTÍCIAS</h2>
+            <nav class="navbar navbar-expand-sm bg-gray navbar-light justify-content-center">
+                <ul class="navbar-nav" style="margin-right: 49%;">
+                    <li class="nav-item active">
+                    <a class="nav-link" href="#">Início</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown-menu">
+                            Noticias
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Colunas</a>
+                            <a class="dropdown-item" href="#">Matérias</a>
+                            <a class="dropdown-item" href="#">Reportagens</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="help.php">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="help.php">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="help.php">Logar</a>
+                    </li>
+                </ul>
+                    <form class="form-inline pull-right" style="margin-right: ;" action="/action_page.php">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Pesquisar" required>
+                    <button class="btn btn-primary" type="submit">Pesquisar</button>
+                </form>
+            </nav>
+        </header>
+        <div class="container-fluid">
+            blabalblablablalbalbalbalbla<br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+        <footer>
+            <div class="row">
+                <div class="col-sm-4 col-footer">Blog de Notícias</div>
+                <div class="col-sm-4 col-footer"><a href="#" target="_blank" title="Página oficial no Facebook" alt="Link externo que redireciona a pagina oficial no Facebook do BlogDeNoticias"><i class="fab fa-facebook fa-lg"></i> Blog de Notícias</a>
+                </div>
+                <div class="col-sm-4 col-footer"><a href="https://github.com/PabloLuiz1/BlogDeNoticias" target="_blank" title="Repositório Git deste projeto" alt="Link externo que redireciona ao projeto git deste site na plataforma online GitHub">
+                    <i class="fas fa-code-branch"></i>Repositório git</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 col-footer"></div>
+                <div class="col-sm-4 col-footer"> <a href="#" target="_blank" title="Canal no YouTube" alt="Link externo que redireciona ao canal do YouTube do BlogDeNoticias">
+                    <i class="fab fa-youtube fa-lg"></i>/blogdenoticias</a>
+                </div>
+                <div class="col-sm-4 col-footer">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 col-footer"></div>
+                <div class="col-sm-4 col-footer"><a href="mailto:contato@blogdenoticias.com" target="_blank" title="E-mail para contato" alt="Link externo que aciona a ação de enviar e-mail">
+                    <i class="fa fa-envelope fa-lg"></i>contato@blogdenoticias.com</a></div>
+                <div class="col-sm-4 col-footer">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 col-footer"></div>
+                <div class="col-sm-4 col-footer"><a href="callto:+5511912345678" target="_blank" title="WhatsApp para contato" alt="Link externo que aciona a ação de adicionar contato">
+                    <i class="fab fa-whatsapp-square fa-lg"></i>+55 11 91234-5678</a></div>
+                <div class="col-sm-4 col-footer">Newsletter - Saiba de cada postagem nova no blog:
+                    <form class="form-inline newsletter" action="#">
+                    <input class="form-control col-sm-8 mr-sm-4" type="text" placeholder="E-mail" required>
+                    <button class="btn btn-success" type="submit">Assinar</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 col-footer"></div>
+                <div class="col-sm-4 col-footer"><span class="copyright">BlogDeNoticias todos os direitos reservados, 2019.
+                    <i class="fa fa-copyright fa-lg"></i></span></div>
+                <div class="col-sm-4 col-footer"></div>
+            </div>
+        </footer>
+    </div>
+</body>
